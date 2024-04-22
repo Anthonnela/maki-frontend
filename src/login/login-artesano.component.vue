74cc0f006654e0b1d6eec371baf8e0d2dc159912
Anthonnela <U20211C403@upc.edu.pe>
2024-04-21 20:36:00 -0500

    feat(login-customer):added api fake

---
+++ src/login/login-artesano.component.vue
@@ -0,0 +1,14 @@
+<script >
+export default {
+  name: 'login-artesano',
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/login/login-customer.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "login-customer"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
