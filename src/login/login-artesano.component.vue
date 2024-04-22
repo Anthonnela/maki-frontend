00c60bd4c44f848c2d5fb05893ba4993d21f7794
Anthonnela <U20211C403@upc.edu.pe>
2024-04-21 20:50:34 -0500

    feat(login-customer):added service to consume api

--- src/login/login-artesano.component.vue
+++ src/login/login-artesano.component.vue
@@ -1,25 +1,7 @@
 
 
 <template>
-  <div class="login-card">
-    <div class="p-d-flex p-jc-center">
-      <img src="/public/client-sign.jpg" alt="Customer Login Image" class="login-image">
-    </div>
-    <div class="p-fluid">
-      <label>Login as Customer</label>
-      <div class="p-field">
-        <label for="email">Email</label>
-        <pv-input-text id="email" v-model="email"></pv-input-text>
-      </div>
-      <div class="p-field">
-        <label for="password">Password</label>
-        <pv-input-text id="password" v-model="password" type="password"></pv-input-text>
-      </div>
-      <div class="p-d-flex p-jc-center">
-        <pv-button @click="login()" class="p-mt-2">Login</pv-button>
-      </div>
-    </div>
-  </div>
+
 </template>
 
 <script >
--- src/login/login-customer.component.vue
+++ src/login/login-customer.component.vue
@@ -1,13 +1,83 @@
-<script>
-export default {
-  name: "login-customer"
-}
-</script>
 
 <template>
-
+  <div class="login-card">
+    <div class="p-d-flex p-jc-center">
+      <img src="/public/client-sign.jpg" alt="Customer Login Image" class="login-image">
+    </div>
+    <div class="p-fluid">
+      <label>Login as Customer</label>
+      <div class="p-field">
+        <label for="email">Email</label>
+        <pv-input-text id="email" v-model="email"></pv-input-text>
+      </div>
+      <div class="p-field">
+        <label for="password">Password</label>
+        <pv-input-text id="password" v-model="password" type="password"></pv-input-text>
+      </div>
+      <div class="p-d-flex p-jc-center">
+        <pv-button @click="login()" class="p-mt-2">Login</pv-button>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+
+import router from "../router.js";
+import {CustomerApiService} from "../services/customer-api.service..js";
+
+export default {
+  name: "Login-Customer",
+  data() {
+    return {
+      email: "",
+      password: "",
+      customerApiService: new CustomerApiService(),
+    };
+  },
+  methods: {
+    async login() {
+      const body = {
+        email: this.email,
+        password: this.password,
+      };
+
+      const response = await this.customerApiService.loginCustomer(
+          body.email,
+          body.password
+      );
+
+      if (response.data.length > 0) {
+        alert("Login successful.");
+        router.push("/home-customer");
+        this.$store.commit("SET_CUSTOMER", response.data[0]);
+      } else {
+        alert("Invalid email or password.");
+      }
+    },
+  },
+};
+</script>
+
+
+
 
 <style scoped>
+.login-card {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  background-color: #f9f9f9;
+}
 
+.login-image {
+  width: 100%;
+  height: auto;
+  border-radius: 8px 8px 0 0;
+}
+
+.p-field {
+  margin-bottom: 20px;
+}
 </style>
